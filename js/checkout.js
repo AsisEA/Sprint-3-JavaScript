d72daadaf02bcbef7f0e11f59c2411f0762189ea
AsisEA <asiselasri@gmail.com>
2021-12-16 18:29:09 +0100

    Nivell 1

--- js/checkout.js
+++ js/checkout.js
@@ -1,14 +1,152 @@
 // Get the input fields
-var password = document.querySelector(".password");
-var phone = document.querySelector('.phone');
-var name = document.querySelector('.name');
+// Nom
+const firstName = document.querySelector('#firstName');
+// Cognom
+const lastName = document.querySelector('#lastName');
+// Correu
+const email = document.querySelector('#email');
+// Contrasenya
+const password = document.querySelector("#password");
+// Adreça
+const address = document.querySelector("#address");
+// Telefon
+const phone = document.querySelector('#phone');
 
 // Get the error elements
-var errorPassword = document.getElementById("errorPassword");
-var errorName = document.getElementById('errorName');  
-var errorPhone = document.getElementById('errorPhone');  
+// Nom
+const errorName = document.querySelector('#errorName');
+const shortName = document.querySelector('#shortName');
+// Cognom
+const errorLastName = document.querySelector('#errorLastName');
+const shortLastName = document.querySelector('#shortLastName');
+// Correu
+const shortEmail = document.querySelector('#shortEmail');
+const notEmail = document.querySelector('#notEmail');
+// Contrasenya
+const errorPassword = document.getElementById("#errorPassword");
+const shortPassword = document.getElementById("#shortPassword");
+// Adreça
+const shortAddress = document.querySelector("#shortAddress");
+const errorPhone = document.getElementById('errorPhone'); 
+
 
 // Exercise 8
-function validate() {
     // Validate fields entered by the user: name, phone, password, and email
+function validate() {
+
+    // Nom
+    firstName.addEventListener("input", function (event) {
+        // Comprova de que hi hagi mes de 3 caracters mirant l'atribut "minlength"
+        if (firstName.validity.tooShort) {
+            firstName.classList.add('is-invalid');
+            shortName.style.display = 'block';
+        } else {
+            firstName.classList.remove('is-invalid');
+            firstName.classList.add('is-valid');
+            shortName.style.display = 'none';
+        }
+        // Comprova que no tingui números mirant l'atribut "Regex"
+        if (firstName.validity.patternMismatch){
+            firstName.classList.add('is-invalid');
+            errorName.style.display = 'block';
+        } else {
+            errorName.style.display = 'none';
+        }
+    });
+
+    // Cognom
+    lastName.addEventListener("input", function (event) {
+        // Comprova de que hi hagi mes de 3 caracters mirant l'atribut "minlength"
+        if (lastName.validity.tooShort) {
+            lastName.classList.add('is-invalid');
+            shortLastName.style.display = 'block';
+    
+        } else {
+            lastName.classList.remove('is-invalid');
+            lastName.classList.add('is-valid');
+            shortLastName.style.display = 'none';
+        }
+        // Comprova que no tingui números mirant l'atribut "Regex"
+        if (lastName.validity.patternMismatch){
+            lastName.classList.add('is-invalid');
+            errorLastName.style.display = 'block';
+        } else {
+            errorLastName.style.display = 'none';
+        }
+    });
+
+    // Email
+    email.addEventListener("input", function (event) {
+        // Comprova de que hi hagi mes de 3 caracters mirant l'atribut "minlength"
+        if (email.validity.tooShort) {
+            email.classList.add('is-invalid');
+            shortEmail.style.display = 'block';
+        } else {
+            email.classList.remove('is-invalid');
+            email.classList.add('is-valid');
+            shortEmail.style.display = 'none';
+        }
+        // Comprova que tingui format d'email mirant l'atribut "Regex"
+        if (email.validity.typeMismatch) {
+            email.classList.add('is-invalid');
+            notEmail.style.display = 'block';
+        } else {
+            notEmail.style.display = 'none';
+        }
+    });
+
+    // Contrasenya
+    password.addEventListener("input", function (event) {
+        // Comprova de que hi hagi mes de 3 caracters mirant l'atribut "minlength"
+        if (password.validity.tooShort) {
+            password.classList.add('is-invalid');
+            shortPassword.style.display = 'block';
+    
+        } else {
+            password.classList.remove('is-invalid');
+            password.classList.add('is-valid');
+            shortPassword.style.display = 'none';
+        }
+        // Comprova que no tingui números i lletres mirant l'atribut "Regex"
+        if (password.validity.patternMismatch){
+            password.classList.add('is-invalid');
+            errorPassword.style.display = 'block';
+        } else {
+            errorPassword.style.display = 'none';
+        }
+    });
+
+    // Adreça
+    address.addEventListener("input", function (event) {
+        // Comprova de que hi hagi mes de 3 caracters mirant l'atribut "minlength"
+        if (address.validity.tooShort) {
+            address.classList.add('is-invalid');
+            shortAddress.style.display = 'block';
+        } else {
+            address.classList.remove('is-invalid');
+            address.classList.add('is-valid');
+            shortAddress.style.display = 'none';
+        }
+    });
+
+    // Telefon
+    phone.addEventListener("input", function (event) {
+        // Comprova de que hi hagi mes de 3 caracters mirant l'atribut "minlength"
+        if (phone.validity.tooShort) {
+            phone.classList.add('is-invalid');
+            shortPhone.style.display = 'block';
+        } else {
+            phone.classList.remove('is-invalid');
+            phone.classList.add('is-valid');
+            shortPhone.style.display = 'none';
+        }
+        // Comprova que no tingui lletres mirant l'atribut "Regex"
+        if (phone.validity.patternMismatch) {
+            phone.classList.add('is-invalid');
+            errorPhone.style.display = 'block';
+        } else {
+            errorPhone.style.display = 'none';
+        }
+    });
+
 }
--- js/shop.js
+++ js/shop.js
@@ -78,43 +78,141 @@ var subtotal = {
 var total = 0;
 
 // Exercise 1
-function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
-}
+function buy(id) {
+
+        // Utilitzo un bucle "for ... in" per recorre l'objecte "products".
+    for (let producte of products) {
+
+        // Si el "id" sigui igual al id de l'arrai fiqui aquest al final de l'arrai "cartList".
+        if (id === products.indexOf(producte) + 1) {
+            cartList.push(producte);
+        }
+    };
+
+        // Comprovació per mostrar la variable"cartList" per consola.
+    // console.log(cartList);
+};
 
 // Exercise 2
 function cleanCart() {
 
-}
+        // Per buidar la llista li dono un valor buit a la variable "cartList" (tal i com estaba a l'inici.).
+    cartList = [];
 
+        // Comprovació per mostrar la variable "cartList" per consola.
+    // console.log(cartList);
+};
+
+    
 // Exercise 3
-function calculateSubtotals() {
     // 1. Create a for loop on the "cartList" array 
     // 2. Implement inside the loop an if...else or switch...case to add the quantities of each type of product, obtaining the subtotals: subtotalGrocery, subtotalBeauty and subtotalClothes
-}
+function calculateSubtotals() {
+
+        // Un bucle "for ... in" que pasa per tots els array de la variable "cartList"
+    for (let producte of cartList) {
+
+        // Si es troba amb algun array on el "type" sigui igual a 'grocery','beauty' o 'clothes' suma el valor de 'price' de la variable 'cartList' al valor que te 'value' en l'arrai corresponent a la variable 'subtotal'.        
+        if (producte.type == 'grocery') {
+            subtotal.grocery.value += producte.price;
+        } else if (producte.type == 'beauty') {
+            subtotal.beauty.value += producte.price;
+        } else if (producte.type == 'clothes') {
+            subtotal.clothes.value += producte.price;
+        };
+    };
+
+        // Comprovació per mostrar la variable "subtotal" per consola.
+    // console.log(subtotal);
+};
 
 // Exercise 4
+    // Calculate total price of the cart either using the "cartList" array  
 function calculateTotal() {
-    // Calculate total price of the cart either using the "cartList" array
-}
+
+        // Un bucle "for ... in" que recorre l'array 'subtotal'
+    for (let producte in subtotal){
+        total += subtotal[producte].value;
+    };
+
+        // Comprovació per mostrar la variable "total" per consola.
+    // console.log(total);
+};
 
 // Exercise 5
-function generateCart() {
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
-}
+function generateCart() {
+
+    for (let producte of cartList){
+
+        // Comprovació de que NO existeix el producte a l'array 'cart' utilitzant '!' per a que en cas de que la comparació sigui positiva dongi negatiu i així afegir el producte de la 'cartList' a 'cart' i a més també afegir els apartats 'quantity' amb el valor 1 i 'subtotalWithDiscount' amb valor 0.
+        if (!cart.some(nouPoducte => nouPoducte.name === producte.name)){
+            cart.push(Object.assign(producte, {quantity: 1, subtotalWithDiscount: 0}));
+
+        // En cas de que ja existeixi el producte en l'array 'cart' suma 1 al valor de l'apartat 'quantity'.
+        } else {
+            for (let nouPoducte of cart){
+                if (producte.name === nouPoducte.name){
+                    nouPoducte.quantity = nouPoducte.quantity + 1;
+                }
+            };
+        }
+    };
+
+        // Comprovació per mostrar la variable "cart" per consola.
+    // console.log(cart);
+};
 
 // Exercise 6
-function applyPromotionsCart() {
     // Apply promotions to each item in the array "cart"
+function applyPromotionsCart() {
+
+        // Amb el 'for ... in' recorrem l'array 'cart'
+    for (let producte of cart){
+
+        // Si el atribut 'name' de cada producte de l'array es igual a 'cooking oil' i a més l'atribut 'quantity' es igual o superior a 3 posa que el preu amb descompte (subtotalWithDiscount) sigui el total (preu per quantitat) menys 10.
+        if (producte.name == 'cooking oil' && producte.quantity >= 3){
+            producte.subtotalWithDiscount = (producte.price * producte.quantity) - 10;
+
+        // Si el atribut 'name' de cada producte de l'array es igual a 'Instant cupcake mixture' i a més l'atribut 'quantity' es igual o superior a 10 posa que el preu amb descompte (subtotalWithDiscount) sigui 1/3 del total total (preu per quantitat).
+        } else if (producte.name == 'Instant cupcake mixture' && producte.quantity >= 10){
+            producte.subtotalWithDiscount = (producte.price * producte.quantity) * (2 / 3);
+        }
+    }
+        // Comprovació per mostrar la variable "cart" per consola.
+    // console.log(cart);
 }
 
 // Exercise 7
-function addToCart(id) {
     // Refactor previous code in order to simplify it 
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cart array or update its quantity in case it has been added previously.
+function addToCart(id) {
+
+//         // Utilitzo un bucle "for ... in" per recorre l'objecte "products".
+//     for (let producte of products){
+
+//         // Comprovació de que el 'id' es igual al de l'arrai.
+        
+//         if (id === products.indexOf(producte) + 1){
+            
+//         // Comprovació de que NO existeix el producte a l'array 'cart' utilitzant '!' per a que en cas de que la comparació sigui positiva dongi negatiu i així afegir el producte de la 'cartList' a 'cart' i a més també afegir els apartats 'quantity' amb el valor 1 i 'subtotalWithDiscount' amb valor 0.
+//             if (!cart.some(nouPoducte => nouPoducte.name === producte.name)){
+//                 cart.push(Object.assign(producte, {quantity: 1, subtotalWithDiscount: 0}));
+
+//         // En cas de que ja existeixi el producte en l'array 'cart' suma 1 al valor de l'apartat 'quantity'.
+//             } else {
+//                 for (nouProducte of cart){
+//                     if (producte.name === nouProducte.name){
+//                         nouPoducte.quantity = nouPoducte.quantity + 1;
+//                     }
+//                 }
+//             }
+//         }
+//     }
 }
 
 // Exercise 9
